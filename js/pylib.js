175624deb05e177b988e4af05daf6b09e8fa9504
Aidan Hobson Sayers <aidanhs@cantab.net>
2013-06-29 22:16:29 +0100

    Prevent closure compiler minifying incorrectly

--- js/pylib.js
+++ js/pylib.js
@@ -1,8 +1,9 @@
 mergeInto(LibraryManager.library, {
+  // emscripten issue 704 - minfies calls to stringify if not object strings
   PyModule_AddIntConstant: function (m, name, value) {
-    root[Module.Pointer_stringify(name)] = value;
+    root[Module["Pointer_stringify"](name)] = value;
   },
   PyModule_AddStringConstant: function (m, name, value) {
-    root[Module.Pointer_stringify(name)] = Module.Pointer_stringify(value);
+    root[Module["Pointer_stringify"](name)] = Module["Pointer_stringify"](value);
   }
 });
