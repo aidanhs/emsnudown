4079114c834a68ed739da83b6b0c2939e3277ba0
Aidan Hobson Sayers <aidanhs@cantab.net>
2013-07-14 21:06:24 +0100

    Fix overaggressive close compiler minification

--- js/postJs.js
+++ js/postJs.js
@@ -12,14 +12,14 @@
     'number','number', // renderer
     'number','number'  // enable_toc
   ]);
-  root['render'] = function (text, options) {
-    if (options === undefined) { options = {}; }
+  root['render'] = function (text, opts) {
+    if (opts === undefined) { opts = {}; }
     return renderWrap(text,
-      options.nofollow      !== undefined ? 1 : 0, options.nofollow,
-      options.target        !== undefined ? 1 : 0, options.target,
-      options.toc_id_prefix !== undefined ? 1 : 0, options.toc_id_prefix,
-      options.renderer      !== undefined ? 1 : 0, options.renderer,
-      options.enable_toc    !== undefined ? 1 : 0, options.enable_toc);
+      opts['nofollow']      !== void 0 ? 1 : 0, opts['nofollow'],
+      opts['target']        !== void 0 ? 1 : 0, opts['target'],
+      opts['toc_id_prefix'] !== void 0 ? 1 : 0, opts['toc_id_prefix'],
+      opts['renderer']      !== void 0 ? 1 : 0, opts['renderer'],
+      opts['enable_toc']    !== void 0 ? 1 : 0, opts['enable_toc']);
   };
   return root;
 
